59ec335b6774953935c0f647e34e3c1304bb7ad3
betsalel koginsky <betsalelkogo@gmail.com>
2023-01-15 18:41:30 +0200

    end course

--- src/routes/post_route.ts
+++ src/routes/post_route.ts
@@ -2,152 +2,143 @@
  * @swagger
  * tags:
  *   name: Post
- *   description: The Post API
+ *   description: The Posts API
  */
+
+import express from "express";
+const router = express.Router();
+import post from "../controllers/post.js";
+import auth from "../controllers/auth.js";
 
 /**
  * @swagger
  * components:
  *   schemas:
- *       Post:
- *           type: object
- *           required:
- *               - message
- *               - sender
- *           properties:
- *               message:
- *                   type: string
- *                   description: The post text
- *               sender:
- *                   type: string
- *                   description: The sender name
- *           example:
- *               message: 'This is new post'
- *               sender: '12345'
+ *     Post:
+ *       type: object
+ *       required:
+ *         - message
+ *         - sender
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: The post text
+ *         sender:
+ *           type: string
+ *           description: The sending user id
+ *       example:
+ *         message: 'this is my new post'
+ *         sender: '12342345234556'
  */
-
-import express from "express";
-const router = express.Router();
-import post from "../controllers/post";
-import auth from "../controllers/auth";
-import NewRequest from "../common/Request";
-import { Request, Response } from "express";
 
 /**
  * @swagger
  * /post:
  *   get:
- *       summary: Get post by id
- *       tags: [Post]
- *       security:
- *           - bearerAuth: []
- *       parameters:
- *         - in: query
- *           name: sender
- *           schema:
- *               type: string
- *               description: Filter posts by sender
- *       responses:
- *           200:
- *               description: Requested post
- *               content:
- *                   application/json:
- *                       schema:
- *                           $ref: '#/components/schemas/Post'
+ *     summary: get list of post from server
+ *     tags: [Post]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: sender
+ *         schema:
+ *           type: string
+ *           description: filter the posts according to the given sender id
+ *     responses:
+ *       200:
+ *         description: the list of posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  $ref: '#/components/schemas/Post'
+ *
  */
-
-//All posts page route
-router.get(
-  "/",
-  auth.authenticateMiddleware,
-  async (req: Request, res: Response) => {
-    try {
-      const response = await post.getAllPosts(NewRequest.fromRestRequest(req));
-      response.sendRestResponse(res);
-    } catch (err) {
-      res.status(400).send({
-        status: "Fail",
-        message: err.message,
-      });
-    }
-  }
-);
+router.get("/", post.getAllPosts);
 
 /**
  * @swagger
  * /post/{id}:
  *   get:
- *       summary: Get post by id
- *       tags: [Post]
- *       security:
- *           - bearerAuth: []
- *       parameters:
- *         - in: path
- *           name: id
- *           required: true
- *           schema:
- *               type: string
- *               description: Filter posts by sender
- *       responses:
- *           200:
- *               description: The new user
- *               content:
- *                   application/json:
- *                       schema:
- *                           type: array
- *                           items:
- *                               $ref: '#/components/schemas/Post'
+ *     summary: get post by id
+ *     tags: [Post]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         requiered: true
+ *         schema:
+ *           type: string
+ *           description: the requested post id
+ *     responses:
+ *       200:
+ *         description: the requested post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *
  */
+router.get("/:id", auth.authenticateMiddleware, post.getPostById);
 
-//Post by id
-router.get(
-  "/:id",
-  auth.authenticateMiddleware,
-  async (req: Request, res: Response) => {
-    try {
-      const response = await post.getPostById(NewRequest.fromRestRequest(req));
-      response.sendRestResponse(res);
-    } catch (err) {
-      res.status(400).send({
-        status: "Fail",
-        message: err.message,
-      });
-    }
-  }
-);
+/**
+ * @swagger
+ * /post:
+ *   post:
+ *     summary: add a new post
+ *     tags: [Post]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: the requested post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *
+ */
+router.post("/", auth.authenticateMiddleware, post.addNewPost);
 
-//New post page route
-router.post(
-  "/",
-  auth.authenticateMiddleware,
-  async (req: Request, res: Response) => {
-    try {
-      const response = await post.addNewPost(NewRequest.fromRestRequest(req));
-      response.sendRestResponse(res);
-    } catch (err) {
-      res.status(400).send({
-        status: "Fail",
-        message: err.message,
-      });
-    }
-  }
-);
-
-//Update post route
-router.put(
-  "/:id",
-  auth.authenticateMiddleware,
-  async (req: Request, res: Response) => {
-    try {
-      const response = await post.updatePost(NewRequest.fromRestRequest(req));
-      response.sendRestResponse(res);
-    } catch (err) {
-      res.status(400).send({
-        status: "Fail",
-        message: err.message,
-      });
-    }
-  }
-);
+/**
+ * @swagger
+ * /post/{id}:
+ *   put:
+ *     summary: update existing post by id
+ *     tags: [Post]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         requiered: true
+ *         schema:
+ *           type: string
+ *           description: the updated post id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: the requested post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *
+ */
+router.put("/:id", auth.authenticateMiddleware, post.putPostById);
 
 export = router;
--- src/server.ts
+++ src/server.ts
@@ -25,12 +25,19 @@ db.once("open", () => {
 });
 
 app.use("/public", express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
 import authRouter from "./routes/auth_route.js";
 app.use("/auth", authRouter);
 
 import postRouter from "./routes/post_route.js";
 app.use("/post", postRouter);
+
+import studentRouter from "./routes/student_route.js";
+app.use("/student", studentRouter);
+
+import fileRouter from "./routes/file_route.js";
+app.use("/file", fileRouter);
 
 import swaggerUI from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
